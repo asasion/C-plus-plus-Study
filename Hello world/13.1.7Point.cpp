ec6ec3e105556e8764cd7682cd79a5ddaec162ad
范希文 <1648355155@qq.com>
2021-09-24 11:04:33 +0800

    laptop

--- Hello world/13.1.7Point.cpp
+++ Hello world/13.1.7Point.cpp
@@ -1,22 +1,22 @@
-#include"1317point.h"
-//点类
-	//设置X坐标
-void Point::setX(int x)//Point作用域下的成员函数，而不是全局函数，所以需要限制以下函数的范围
-{
-	m_X = x;
-}
-//获取X坐标
-int Point::getX()
-{
-	return m_X;
-}
-//设置Y坐标
-void Point::setY(int y)
-{
-	m_X = y;
-}
-//设置Y坐标
-int Point::getY()
-{
-	return m_Y;
-}
+//#include"1317point.h"
+////点类
+//	//设置X坐标
+//void Point::setX(int x)//Point作用域下的成员函数，而不是全局函数，所以需要限制以下函数的范围
+//{
+//	m_X = x;
+//}
+////获取X坐标
+//int Point::getX()
+//{
+//	return m_X;
+//}
+////设置Y坐标
+//void Point::setY(int y)
+//{
+//	m_X = y;
+//}
+////设置Y坐标
+//int Point::getY()
+//{
+//	return m_Y;
+//}
